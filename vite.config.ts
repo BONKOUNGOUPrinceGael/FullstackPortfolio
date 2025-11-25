bb5111c7d3e5698d1028b6159aadc74d2b56a966
BONKOUNGOUPrinceGael <princegaelb@gmail.com>
2025-11-25 23:25:47 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base:process.env.VITE_BASE_PATH || "/Portfolio Fullstack",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
